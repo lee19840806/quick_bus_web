3c5ee2e535df2225c8ab50062cedfef21096cc1f
lee19840806 <leegao36@163.com>
2014-04-02 21:32:06 +0800

    数据库的小更新
    更新了View：view_user_route_summary

--- database_dump/Dump_quick_bus_alpha.sql
+++ database_dump/Dump_quick_bus_alpha.sql
@@ -61,7 +61,7 @@ CREATE TABLE `user_route_points` (
   UNIQUE KEY `UK_user_route_points_id` (`id`),
   KEY `FK_user_route_points_user_routes_id` (`user_route_id`),
   CONSTRAINT `FK_user_route_points_user_routes_id` FOREIGN KEY (`user_route_id`) REFERENCES `user_routes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +70,7 @@ CREATE TABLE `user_route_points` (
 
 LOCK TABLES `user_route_points` WRITE;
 /*!40000 ALTER TABLE `user_route_points` DISABLE KEYS */;
-INSERT INTO `user_route_points` VALUES (24,7,1,31.22461918,121.55526638),(25,7,2,31.21551746,121.55775547),(26,7,3,31.21727915,121.56925678),(27,7,4,31.21001197,121.57123089),(28,8,1,31.20869060,121.53531075),(29,8,2,31.20178983,121.54303551),(30,8,3,31.18974899,121.54887199),(31,8,4,31.17080358,121.55608177),(32,8,5,31.16551582,121.55934334),(33,8,6,31.15023839,121.56260490);
+INSERT INTO `user_route_points` VALUES (28,8,1,31.20869060,121.53531075),(29,8,2,31.20178983,121.54303551),(30,8,3,31.18974899,121.54887199),(31,8,4,31.17080358,121.55608177),(32,8,5,31.16551582,121.55934334),(33,8,6,31.15023839,121.56260490),(34,9,1,31.13716248,121.57311916),(35,9,2,31.13672169,121.56316280),(36,9,3,31.13628089,121.55346394),(37,9,4,31.13510543,121.54745579),(38,9,5,31.13826445,121.54625416),(39,9,6,31.15486595,121.54196262),(40,9,7,31.16713155,121.54161930),(41,9,8,31.17131765,121.54144764),(42,9,9,31.18152509,121.53784275),(43,9,10,31.18996926,121.53595448),(44,9,11,31.19944052,121.53380871),(45,9,12,31.20443274,121.53398037),(46,9,13,31.20891083,121.53552532),(47,9,14,31.21426958,121.53767109),(48,9,15,31.21830679,121.54033184),(49,9,16,31.21955462,121.54213428),(50,9,17,31.22058223,121.54058933);
 /*!40000 ALTER TABLE `user_route_points` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -91,7 +91,7 @@ CREATE TABLE `user_routes` (
   UNIQUE KEY `UK_user_routes_id` (`id`),
   KEY `FK_user_routes_user_accounts_id` (`user_id`),
   CONSTRAINT `FK_user_routes_user_accounts_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,7 +100,7 @@ CREATE TABLE `user_routes` (
 
 LOCK TABLES `user_routes` WRITE;
 /*!40000 ALTER TABLE `user_routes` DISABLE KEYS */;
-INSERT INTO `user_routes` VALUES (7,6,'r1',NULL,NULL),(8,6,'沪南路1','2014-03-30 12:57:54','2014-03-30 12:57:54');
+INSERT INTO `user_routes` VALUES (8,6,'沪南路1','2014-03-30 12:57:54','2014-03-30 12:57:54'),(9,6,'锦绣路','2014-04-02 12:43:18','2014-04-02 12:43:18');
 /*!40000 ALTER TABLE `user_routes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,7 +121,7 @@ CREATE TABLE `user_station_points` (
   UNIQUE KEY `UK_user_station_points_id` (`id`),
   KEY `FK_user_station_points_user_routes_id` (`user_route_id`),
   CONSTRAINT `FK_user_station_points_user_routes_id` FOREIGN KEY (`user_route_id`) REFERENCES `user_routes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,7 +130,7 @@ CREATE TABLE `user_station_points` (
 
 LOCK TABLES `user_station_points` WRITE;
 /*!40000 ALTER TABLE `user_station_points` DISABLE KEYS */;
-INSERT INTO `user_station_points` VALUES (15,7,1,31.21867380,121.55689716),(16,7,2,31.21654512,121.56384945),(17,7,3,31.21338871,121.57037258),(18,8,1,31.19576960,121.54612541),(19,8,2,31.17315361,121.55522346),(20,8,3,31.15141366,121.56226158);
+INSERT INTO `user_station_points` VALUES (18,8,1,31.19576960,121.54612541),(19,8,2,31.17315361,121.55522346),(20,8,3,31.15141366,121.56226158),(21,9,1,31.14641865,121.54402256),(22,9,2,31.18174538,121.53767109),(23,9,3,31.19929369,121.53380871),(24,9,4,31.21441639,121.53784275);
 /*!40000 ALTER TABLE `user_station_points` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,7 +150,7 @@ CREATE TABLE `user_trigger_points` (
   UNIQUE KEY `UK_user_trigger_points_id` (`id`),
   KEY `FK_user_trigger_points_user_station_points_id` (`user_station_id`),
   CONSTRAINT `FK_user_trigger_points_user_station_points_id` FOREIGN KEY (`user_station_id`) REFERENCES `user_station_points` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -159,7 +159,7 @@ CREATE TABLE `user_trigger_points` (
 
 LOCK TABLES `user_trigger_points` WRITE;
 /*!40000 ALTER TABLE `user_trigger_points` DISABLE KEYS */;
-INSERT INTO `user_trigger_points` VALUES (7,15,31.22300442,121.55578136),(8,16,31.21559087,121.55938625),(9,17,31.21683873,121.56745434),(10,18,31.20428591,121.54046059),(11,19,31.18637140,121.55024529),(12,20,31.16860038,121.55762672);
+INSERT INTO `user_trigger_points` VALUES (10,18,31.20428591,121.54046059),(11,19,31.18637140,121.55024529),(12,20,31.16860038,121.55762672),(13,21,31.13679515,121.56608105),(14,22,31.14612481,121.54410839),(15,23,31.17337392,121.54076099),(16,24,31.18952871,121.53604031);
 /*!40000 ALTER TABLE `user_trigger_points` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -245,6 +245,7 @@ DROP TABLE IF EXISTS `view_user_route_detail`;
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 /*!50001 CREATE TABLE `view_user_route_detail` (
+  `user_id` tinyint NOT NULL,
   `username` tinyint NOT NULL,
   `user_route_id` tinyint NOT NULL,
   `name` tinyint NOT NULL,
@@ -265,9 +266,11 @@ DROP TABLE IF EXISTS `view_user_route_summary`;
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 /*!50001 CREATE TABLE `view_user_route_summary` (
+  `user_id` tinyint NOT NULL,
   `username` tinyint NOT NULL,
   `user_route_id` tinyint NOT NULL,
   `name` tinyint NOT NULL,
+  `modified` tinyint NOT NULL,
   `route_point_cnt` tinyint NOT NULL,
   `station_cnt` tinyint NOT NULL,
   `trigger_cnt` tinyint NOT NULL
@@ -353,7 +356,7 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = utf8_general_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `view_user_route_detail` AS select `a`.`username` AS `username`,`b`.`id` AS `user_route_id`,`b`.`name` AS `name`,`c`.`sequence` AS `station_sequence`,`c`.`longitude` AS `station_lng`,`c`.`latitude` AS `station_lat`,`d`.`longitude` AS `trigger_lng`,`d`.`latitude` AS `trigger_lat` from (((`users` `a` left join `user_routes` `b` on((`a`.`id` = `b`.`user_id`))) left join `user_station_points` `c` on((`b`.`id` = `c`.`user_route_id`))) left join `user_trigger_points` `d` on((`c`.`id` = `d`.`user_station_id`))) order by `a`.`username`,`b`.`id`,`c`.`sequence` */;
+/*!50001 VIEW `view_user_route_detail` AS select `a`.`id` AS `user_id`,`a`.`username` AS `username`,`b`.`id` AS `user_route_id`,`b`.`name` AS `name`,`c`.`sequence` AS `station_sequence`,`c`.`longitude` AS `station_lng`,`c`.`latitude` AS `station_lat`,`d`.`longitude` AS `trigger_lng`,`d`.`latitude` AS `trigger_lat` from (((`users` `a` left join `user_routes` `b` on((`a`.`id` = `b`.`user_id`))) left join `user_station_points` `c` on((`b`.`id` = `c`.`user_route_id`))) left join `user_trigger_points` `d` on((`c`.`id` = `d`.`user_station_id`))) order by `a`.`username`,`b`.`id`,`c`.`sequence` */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -372,7 +375,7 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = utf8_general_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `view_user_route_summary` AS select `a`.`username` AS `username`,`b`.`id` AS `user_route_id`,`b`.`name` AS `name`,`c`.`route_point_cnt` AS `route_point_cnt`,`d`.`station_cnt` AS `station_cnt`,`e`.`trigger_cnt` AS `trigger_cnt` from ((((`users` `a` left join `user_routes` `b` on((`a`.`id` = `b`.`user_id`))) left join `view_subquery_route_point_cnt` `c` on((`b`.`id` = `c`.`user_route_id`))) left join `view_subquery_station_cnt` `d` on((`b`.`id` = `d`.`user_route_id`))) left join `view_subquery_trigger_cnt` `e` on((`b`.`id` = `e`.`user_route_id`))) order by `a`.`username`,`b`.`id` */;
+/*!50001 VIEW `view_user_route_summary` AS select `a`.`id` AS `user_id`,`a`.`username` AS `username`,`b`.`id` AS `user_route_id`,`b`.`name` AS `name`,`b`.`modified` AS `modified`,`c`.`route_point_cnt` AS `route_point_cnt`,`d`.`station_cnt` AS `station_cnt`,`e`.`trigger_cnt` AS `trigger_cnt` from ((((`users` `a` left join `user_routes` `b` on((`a`.`id` = `b`.`user_id`))) left join `view_subquery_route_point_cnt` `c` on((`b`.`id` = `c`.`user_route_id`))) left join `view_subquery_station_cnt` `d` on((`b`.`id` = `d`.`user_route_id`))) left join `view_subquery_trigger_cnt` `e` on((`b`.`id` = `e`.`user_route_id`))) order by `a`.`username`,`b`.`id` */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -386,4 +389,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-03-30 22:38:25
+-- Dump completed on 2014-04-02 21:27:41
