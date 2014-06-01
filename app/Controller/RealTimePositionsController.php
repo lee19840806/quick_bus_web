30bb8e982062a1211d7144f6e9cd68518ce8debd
lee19840806 <leegao36@163.com>
2014-06-01 10:59:30 +0800

    数据库Bug修改，重复发送短信Bug的修复
    同一个站点同一个手机号会被多次发送短信的Bug已修复

--- app/Controller/RealTimePositionsController.php
+++ app/Controller/RealTimePositionsController.php
@@ -63,6 +63,51 @@ class RealTimePositionsController extends AppController {
         $this->render('/RealTimePositions/upload', 'ajax');
     }
     
+    public function no_sms()
+    {
+        if ($this->request->is('post'))
+        {
+            $returnValue = $this->RealTimePosition->saveRealTimePosition($this->request->data['RealTimePosition'], $this->Auth->user('id'));
+            $this->set('returnValue', $returnValue);
+        }
+        else
+        {
+            $this->set('returnValue', 99);
+        }
+        
+        $notifyPhones = $this->RealTimePosition->ViewUserNotifyPhone->find('all', array(
+            'conditions' => array(
+                'ViewUserNotifyPhone.user_id' => $this->Auth->user('id'),
+                'ViewUserNotifyPhone.user_route_id' => $this->request->data['RealTimePosition']['user_route_id']),
+            'fields' => array('ViewUserNotifyPhone.*')
+            ));
+        
+        if (count($notifyPhones) > 0)
+        {
+            $UserNotifyPhoneHistoryRecords = array();
+            $phoneNumbersArray = array();
+            $stationName = '';
+            
+            foreach ($notifyPhones as $phone)
+            {
+                array_push($UserNotifyPhoneHistoryRecords, $phone['ViewUserNotifyPhone']);
+                array_push($phoneNumbersArray, $phone['ViewUserNotifyPhone']['phone_number']);
+                $stationName = $phone['ViewUserNotifyPhone']['station_name'];
+                
+                $this->set('returnValue', $returnValue);
+            }
+            
+            $this->RealTimePosition->UserNotifyPhoneHistory->create();
+            
+            if (!$this->RealTimePosition->UserNotifyPhoneHistory->saveMany($UserNotifyPhoneHistoryRecords))
+            {
+                $this->set('returnValue', 4);
+            }
+        }
+        
+        $this->render('/RealTimePositions/upload', 'ajax');
+    }
+    
     public function test_upload()
     {
         
--- test/tests/api/UploadCept.php
+++ test/tests/api/UploadCept.php
@@ -7,32 +7,11 @@ $I->fillField('data[User][password]', 'abc123');
 $I->click('LogIn');
 $I->see('查看');
 
-$I->sendPOST('RealTimePositions/upload', array('data[RealTimePosition][user_id]' => 7, 'data[RealTimePosition][user_route_id]' => 28,
-    'data[RealTimePosition][latitude]' => 31.21889, 'data[RealTimePosition][longitude]' => 121.54458, 'data[RealTimePosition][heading]' => 201));
+$I->sendPOST('RealTimePositions/no_sms', array('data[RealTimePosition][user_id]' => 7, 'data[RealTimePosition][user_route_id]' => 29,
+    'data[RealTimePosition][latitude]' => 31.14491882, 'data[RealTimePosition][longitude]' => 121.56331525, 'data[RealTimePosition][heading]' => 1));
 
 sleep(4);
 
-//$I->sendPOST('RealTimePositions/upload', array('data[RealTimePosition][user_id]' => 7, 'data[RealTimePosition][user_route_id]' => 28,
-//    'data[RealTimePosition][latitude]' => 31.20889, 'data[RealTimePosition][longitude]' => 121.53549, 'data[RealTimePosition][heading]' => 201));
-/*
-sleep(5);
+$I->sendPOST('RealTimePositions/no_sms', array('data[RealTimePosition][user_id]' => 7, 'data[RealTimePosition][user_route_id]' => 29,
+    'data[RealTimePosition][latitude]' => 31.14575380, 'data[RealTimePosition][longitude]' => 121.56335576, 'data[RealTimePosition][heading]' => 359));
 
-$I->sendPOST('RealTimePositions/upload', array('data[RealTimePosition][user_id]' => 7, 'data[RealTimePosition][user_route_id]' => 28,
-    'data[RealTimePosition][latitude]' => 31.19967, 'data[RealTimePosition][longitude]' => 121.53364, 'data[RealTimePosition][heading]' => 180));
-
-sleep(5);
-
-$I->sendPOST('RealTimePositions/upload', array('data[RealTimePosition][user_id]' => 7, 'data[RealTimePosition][user_route_id]' => 28,
-    'data[RealTimePosition][latitude]' => 31.19959, 'data[RealTimePosition][longitude]' => 121.53364, 'data[RealTimePosition][heading]' => 179));
-
-sleep(5);
-
-$I->sendPOST('RealTimePositions/upload', array('data[RealTimePosition][user_id]' => 7, 'data[RealTimePosition][user_route_id]' => 28, 
-    'data[RealTimePosition][latitude]' => 31.19931, 'data[RealTimePosition][longitude]' => 121.53365, 'data[RealTimePosition][heading]' => 168));
-
-sleep(5);
-
-$I->sendPOST('RealTimePositions/upload', array('data[RealTimePosition][user_id]' => 7, 'data[RealTimePosition][user_route_id]' => 28,
-    'data[RealTimePosition][latitude]' => 31.19511, 'data[RealTimePosition][longitude]' => 121.53459, 'data[RealTimePosition][heading]' => 168));
-*/
-$I->see('0');
