fcb3e5a8ba595e25d2d3a4669f415ddc13d2ba80
lee19840806 <leegao36@163.com>
2014-12-30 23:51:52 +0800

    最后一个站点坐标查询接口

--- app/Controller/ViewUserRouteDetailsController.php
+++ app/Controller/ViewUserRouteDetailsController.php
@@ -32,4 +32,35 @@ class ViewUserRouteDetailsController extends AppController {
 			$this->render('/ViewUserRouteDetails/ajaxReturn', 'ajax');
 		}
 	}
+	
+	public function wechat_route_ending_station()
+	{
+		if ($this->request->is('post'))
+		{
+			$max_station_seq = $this->ViewUserRouteDetail->find('first',
+				array('conditions' => array(
+				'ViewUserRouteDetail.user_id' => $this->Auth->user('id'),
+				'ViewUserRouteDetail.user_route_id' => $this->request->data('user_route_id')
+				),
+				'fields' => array('max(station_sequence) as max_station_seq')
+				));
+			
+			$route_detail = $this->ViewUserRouteDetail->find('first',
+				array('conditions' => array(
+				'ViewUserRouteDetail.user_id' => $this->Auth->user('id'),
+				'ViewUserRouteDetail.user_route_id' => $this->request->data('user_route_id'),
+				'ViewUserRouteDetail.station_sequence' => $max_station_seq[0]['max_station_seq']
+				),
+				'fields' => array('user_route_id', 'route_name', 'station_sequence', 'station_name', 'station_lng', 'station_lat')
+				));
+	
+			$station_array = array('linename' => $route_detail['ViewUserRouteDetail']['route_name'], 
+				'lo' => $route_detail['ViewUserRouteDetail']['station_lng'],
+				'la' => $route_detail['ViewUserRouteDetail']['station_lat']
+				);
+			
+			$this->set('route_detail', json_encode(array($this->request->data('user_route_id') => $station_array)));
+			$this->render('/ViewUserRouteDetails/ajaxReturn', 'ajax');
+		}
+	}
 }
