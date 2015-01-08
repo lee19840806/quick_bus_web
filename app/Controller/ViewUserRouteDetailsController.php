90f0eb271f4fe36616ebbd87db7b7629716a0f9e
lee19840806 <leegao36@163.com>
2015-01-08 21:00:35 +0800

    wechat_route_station_detail接口不需要登录也能查询

--- app/Controller/ViewUserRouteDetailsController.php
+++ app/Controller/ViewUserRouteDetailsController.php
@@ -16,13 +16,18 @@ class ViewUserRouteDetailsController extends AppController {
  */
 	public $components = array('Paginator', 'Session');
 	
+	public function beforeFilter()
+	{
+		parent::beforeFilter();
+		$this->Auth->allow('wechat_route_station_detail');
+	}
+	
 	public function wechat_route_station_detail()
 	{
 		if ($this->request->is('post'))
 		{
 			$route_detail = $this->ViewUserRouteDetail->find('all',
 				array('conditions' => array(
-				'ViewUserRouteDetail.user_id' => $this->Auth->user('id'),
 				'ViewUserRouteDetail.user_route_id' => $this->request->data('user_route_id')
 				),
 				'fields' => array('user_route_id', 'route_name', 'station_sequence', 'station_name', 'station_lng', 'station_lat')
